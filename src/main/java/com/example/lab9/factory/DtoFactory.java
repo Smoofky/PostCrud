d73e139d06855177ad2d87c7fe65ec396a432d1a
Mikołaj Starczewski <83308784+31grudnia@users.noreply.github.com>
2024-03-07 23:40:47 +0100

    Update DtoFactory.java

--- src/main/java/com/example/lab9/factory/DtoFactory.java
+++ src/main/java/com/example/lab9/factory/DtoFactory.java
@@ -5,7 +5,11 @@ import com.example.lab9.model.Post;
 import com.example.lab9.dto.CommentDto;
 import com.example.lab9.model.Comment;
 
+
+
+// Tydzień 1, Wzorzec Fabryka 1/4
 public interface DtoFactory {
     PostDto createPostDto(Post post);
     CommentDto createCommentDto(Comment comment);
 }
+// Tydzień 1, Wzorzec Fabryka 1/4 KONIEC
