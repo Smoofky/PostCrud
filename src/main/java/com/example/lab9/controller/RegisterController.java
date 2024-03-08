2a5dac4876c5f961f1bec92ac52699af8874dfe9
ktszewczyk <107799708+ktszewczyk@users.noreply.github.com>
2024-03-08 13:26:45 +0100

    Update RegisterController.java

--- src/main/java/com/example/lab9/controller/RegisterController.java
+++ src/main/java/com/example/lab9/controller/RegisterController.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.example.lab9.controller;
 
 import com.example.lab9.dto.UserDto;
@@ -13,44 +9,42 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-/**
- *
- * @author rytel
- */
 @Controller
 @CrossOrigin
 public class RegisterController {
 
-    @Autowired
-    private JwtUserDetailsService userDetailsService;
+    
+  //Tydzień 2, Stosowanie konstrukcyjnych wzorców projektowych, Wzorzec Singleton
+    //W tym kodzie zastosowano technikę Singleton poprzez leniwą inicjalizację (Lazy Initialization) 
+    //z synchronizacją za pomocą metody getInstance().
+    //Użycie!
+    
+    
+    // Użycie Singletona JwtUserDetailsService
+    private final JwtUserDetailsService userDetailsService = JwtUserDetailsService.getInstance();
 
     @GetMapping("/register")
-    public String showRegistrationForm(Model model, @RequestParam(value = "usernameTaken", required = false) String usernameTaken) {
+    public String showRegistrationForm(Model model) {
         model.addAttribute("user", new UserDto());
-        model.addAttribute("usernameTaken", usernameTaken);
         return "register";
     }
 
     @PostMapping("/register")
     public String registerUser(@ModelAttribute("user") UserDto user, Model model) {
         try {
-            // Check if the username is already taken
+            // Sprawdzenie, czy nazwa użytkownika jest zajęta
             if (userDetailsService.isUsernameTaken(user.getUsername())) {
                 model.addAttribute("error", "Username is already taken. Please choose a different one.");
-                return "redirect:/register?usernameTaken=true";
+                return "redirect:/register";
             }
-
-            // Save the user
+           //Zapisanie nowego użytkownika
             userDetailsService.save(user);
-
-            // Redirect to the login page with a success message
             return "redirect:/login?success=registered";
         } catch (Exception e) {
-            // Redirect to the login page with an error message
             model.addAttribute("error", "Something went wrong. Please try again.");
             return "register";
         }
     }
 }
+//Koniec, Tydzień 2, Wzorzec Singleton.
